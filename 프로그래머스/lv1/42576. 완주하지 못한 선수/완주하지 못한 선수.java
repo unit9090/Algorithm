e07bfa786e95737b4f262e93e263fa3c2ca62a7e
unit <93903643+unit9090@users.noreply.github.com>
2023-08-27 23:31:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.49 ms, Memory: 94.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String s : participant) {
+            if(map.get(s) == null) {
+                map.put(s, 1);
+            } else {
+                map.put(s, map.get(s) + 1);
+            }            
+        }
+        
+        for(String s : completion) {
+            if(map.get(s) == 1) { map.remove(s); }
+            else {
+                map.put(s, map.get(s) - 1);
+            }
+        }
+        
+        for(String s : map.keySet()) answer = s;
+        
+        return answer;
+    }
+    
+}
