bcc5e2cebc75dc4a73bf263a7c5588d5852ad68c
unit <93903643+unit9090@users.noreply.github.com>
2023-08-29 00:21:27 +0900

    [Silver IV] Title: 주몽, Time: 208 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        
+        int[] A = new int[N];        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+        	A[i] = Integer.parseInt(st.nextToken());
+        }
+        // 오름차순 정렬
+        Arrays.sort(A);
+        
+        int count = 0;
+        int i = 0;			// A[0] -> Min
+        int j = N - 1;		// A[N - 1] -> Max
+        
+        while(i < j) {
+        	if(A[i] + A[j] < M) i++;
+            else if(A[i] + A[j] > M) j--;
+            else {
+            	count++;
+                i++; j--;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
