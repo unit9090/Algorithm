7dbe0c4efbce54765827946fff4f782274146fd4
unit <93903643+unit9090@users.noreply.github.com>
2023-08-29 00:20:45 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        
+        if(A > 0 && B < 10) {
+            int sum = A - B;
+            System.out.println(sum);
+        }
+    }
+}
