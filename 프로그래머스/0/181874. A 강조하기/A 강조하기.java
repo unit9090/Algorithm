a381a48e09aa82195da262a5e001f582be91e1ad
unit <93903643+unit9090@users.noreply.github.com>
2024-08-10 00:29:31 +0900

    [level 0] Title: A 강조하기, Time: 0.11 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        myString = myString.toLowerCase();
+        answer = myString.replaceAll("a", "A");
+        
+        return answer;
+    }
+}
