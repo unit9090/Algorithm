6254354f816643e597ed1d8862ea404053d9ba2e
unit <93903643+unit9090@users.noreply.github.com>
2025-03-30 18:40:19 +0900

    [level 0] Title: 코드 처리하기, Time: 12.89 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,22 +1,34 @@
 class Solution {
     public String solution(String code) {
-        String ret = "";
+        //String ret = "";
         Boolean mode = false;
         
-        for(int i = 0; i < code.length(); i++) {
-            char ch = code.charAt(i);
+//         for(int i = 0; i < code.length(); i++) {
+//             char ch = code.charAt(i);
             
-            if(mode) {
-                if(ch != '1' && i % 2 != 0) ret += ch;
-                else if(ch == '1') mode = false;
-            } else {
-                if(ch != '1' && i % 2 == 0) ret += ch;
-                else if(ch == '1') mode = true;
+//             if(mode) {
+//                 if(ch != '1' && i % 2 != 0) ret += ch;
+//                 else if(ch == '1') mode = false;
+//             } else {
+//                 if(ch != '1' && i % 2 == 0) ret += ch;
+//                 else if(ch == '1') mode = true;
+//             }
+//         }
+        
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < code.length(); i++) {
+            if(code.charAt(i) == '1') {
+                mode = !mode;
+                continue;
             }
+            
+            if((mode && i % 2 != 0) || (!mode && i % 2 == 0))
+                sb.append(code.charAt(i));   
         }
         
-        if(ret.equals("")) ret = "EMPTY";
+        //if(ret.equals("")) ret = "EMPTY";
+        if(sb.length() <= 0) sb.append("EMPTY");
         
-        return ret;
+        return sb.toString();
     }
 }
